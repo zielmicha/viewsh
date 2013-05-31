610a41a7e71ec9508400b7b3c1a36443541b4f44
Michał Zieliński <michal@zielinscy.org.pl>
2013-05-31 22:39:17 +0200

    moved main loop to viewsh/rc.py

--- viewsh/commands.py
+++ viewsh/commands.py
@@ -5,7 +5,7 @@ from viewsh.executor import Executor
 def vssh(state, terminal, userhost):
     user, host = userhost.split('@')
     transport = ssh.SSHTransport(host, user, parent=state[Transport])
-    state[Transport] = transport # TODO: .switch_transport
+    state[SwitchTransport].switch(transport)
 
 def register(state):
     state[Executor].commands['vssh'] = vssh
--- viewsh/rc.py
+++ viewsh/rc.py
@@ -1,6 +1,7 @@
 from viewsh import terminal
 from viewsh.prompt import PS1
 from viewsh.executor import Executor
+from viewsh.transport import Transport
 
 import os
 
@@ -29,3 +30,30 @@ def default_rc(state):
 
     from viewsh import commands
     commands.register(state)
+
+def main():
+    # Assemble everything.
+    from viewsh import terminal
+    from viewsh import rc
+    from viewsh import state as shell_state
+    from viewsh.transport import local
+    from viewsh import comm
+    from viewsh import edit
+    from viewsh import prompt
+
+    terminal = terminal.Terminal()
+    terminal.start()
+    state = shell_state.ShellState()
+    state[Transport] = local.LocalTransport()
+    state[comm.Interface].patch_log()
+    setup(state)
+    prompt = prompt.Prompt(state, terminal)
+
+    while True:
+        prompt.show()
+        line_edit = edit.LineEdit(state, terminal)
+        line = line_edit.prompt()
+        state[Executor].execute(terminal, line)
+
+if __name__ == '__main__':
+    main()
